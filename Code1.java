2faf13bc1dd9edabaecf8e046d84065c3b8062b0
nithishreddy1 <nithishreddy307@gmail.com>
2025-08-08 12:35:00 -0400

    Sending updates of Code1 and Code2

---
+++ Code1.java
@@ -0,0 +1,5 @@
+Public class Code1{
+  public static void main(String args[]){
+    System.out.println("This is my first part of Code")
+  }
+}
---
+++ Code2.java
@@ -0,0 +1,5 @@
+Public class Code2{
+  public static void main(String args[]){
+    System.out.println("This is my second part of code");
+  }
+}
